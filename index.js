c8e13726889ea3636611e2abde5d98b9858d91a7
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-16 00:06:14 +0600

    add some  element

--- index.js
+++ index.js
@@ -120,6 +120,21 @@ async function run(){
         //     await bookingsCollection.insertOne(booking);            
         //     res.send(result);
         // })
+
+        app.get('/bookings', /**verifyJWT, */  async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
