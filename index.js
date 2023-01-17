6500837ced87634b8bf9ded42961ec11002508db
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-18 00:10:56 +0600

    jwt add function

--- index.js
+++ index.js
@@ -15,7 +15,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next) {
+    console.log("token",req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
 
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 async function run(){
     try{
@@ -122,13 +139,14 @@ async function run(){
         //     res.send(result);
         // })
 
-        app.get('/bookings', /**verifyJWT, */  async (req, res) => {
+        app.get('/bookings', verifyJWT,   async (req, res) => {
             const email = req.query.email;
-            // const decodedEmail = req.decoded.email;
+            
+            const decodedEmail = req.decoded.email;
 
-            // if (email !== decodedEmail) {
-            //     return res.status(403).send({ message: 'forbidden access' });
-            // }
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
 
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
