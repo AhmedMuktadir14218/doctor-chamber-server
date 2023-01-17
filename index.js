4e876a29b89254959b488ba6dfce87fc2ca5e552
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-17 23:45:26 +0600

    add token in env

--- index.js
+++ index.js
@@ -162,11 +162,11 @@ async function run(){
             const query = { email: email };
             const user = await usersCollection.findOne(query);
             
-            // if (user) {
-            //     const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-            //     return res.send({ accessToken: token });
-            // }
-            console.log(user);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            // console.log(user);
             res.status(403).send({ accessToken: '' })
             // res.send({ accessToken: 'sdfhdfnhdf' })
         });
