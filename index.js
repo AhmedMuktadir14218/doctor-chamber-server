081393c30da84f5ff4bedbb5c9f2ef7c5141c503
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-19 20:27:18 +0600

    jwt ganjam running

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
