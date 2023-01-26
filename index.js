187975f7f0b029afac3891b80757b4a8a502c02a
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-26 21:38:19 +0600

    jwt

--- index.js
+++ index.js
@@ -316,17 +316,17 @@ async function run() {
         // });
 
         // temporary to update price field on appointment options
-        // app.get('/addPrice', async (req, res) => {
-        //     const filter = {}
-        //     const options = { upsert: true }
-        //     const updatedDoc = {
-        //         $set: {
-        //             price: 99
-        //         }
-        //     }
-        //     const result = await appointmentOptionCollection.updateMany(filter, updatedDoc, options);
-        //     res.send(result);
-        // })
+        app.get('/addPrice', async (req, res) => {
+            const filter = {}
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    price: 99
+                }
+            }
+            const result = await appointmentOptionCollection.updateMany(filter, updatedDoc, options);
+            res.send(result);
+        })
 
 
       
